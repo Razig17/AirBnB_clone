4474f7da77ca030355ddda0796c8252e09ea9863
Razig17 <abdelrazig.sharif1@gmail.com>
2024-03-24 02:45:12 +0300

    Update base_model file

--- models/base_model.py
+++ models/base_model.py
@@ -47,5 +47,4 @@ class BaseModel:
         my_dict["__class__"] = self.__class__.__name__
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
-
         return my_dict
