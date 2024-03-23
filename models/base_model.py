13ea76acba0791479598e22da776f20d6ec0d649
Razig17 <abdelrazig.sharif1@gmail.com>
2024-03-24 02:44:46 +0300

    Update base_model file

--- models/base_model.py
+++ models/base_model.py
@@ -5,9 +5,10 @@ This module contains BaseModule class that
 defines all common attributes/methods for other classes
 """
 
-from datetime import datetime
+
 import models
 import uuid
+from datetime import datetime
 
 
 class BaseModel:
@@ -19,6 +20,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
@@ -29,7 +31,7 @@ class BaseModel:
             models.storage.new(self)
 
     def __str__(self):
-        """Returns [<class name>] (<self.id>) <self.__dict__>"""
+        """Returns The string representation of the class"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
