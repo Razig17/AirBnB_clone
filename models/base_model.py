869151ce571e6765acf6bf2c4803c73749ae1f34
Razig17 <abdelrazig.sharif1@gmail.com>
2024-03-23 15:24:32 +0300

    Update base_model file

--- models/base_model.py
+++ models/base_model.py
@@ -11,12 +11,13 @@ import uuid
 
 
 class BaseModel():
+
     """Defines all common attributes/methods for other classes"""
 
     def __init__(self, *args, **kwargs):
         """Initialize instances of the BaseModel class"""
 
-        self.id = str(uuid.uuid4)
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         if kwargs:
