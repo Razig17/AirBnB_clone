30d7918fcf99cb9eb60f060480988a92ba49d7a8
Razig17 <abdelrazig.sharif1@gmail.com>
2024-03-23 23:50:58 +0300

    Update  file_storage file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,7 +4,7 @@ This module contains class FileStorage that serializes instances to a JSON file
 and deserializes JSON file to instances
 """
 
-
+import os
 import json
 from models.base_model import BaseModel
 
@@ -17,34 +17,41 @@ class FileStorage():
     """
 
     __file_path = "file.json"
-    ___objects = {}
+    __objects = {}
 
     def all(self):
 
         """ Returns the dictionary __objects"""
-        return self.___objects
+        return self.__objects
 
     def new(self, obj):
 
         """sets in __objects the obj with key <obj class name>.id"""
 
-        self.___objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
 
     def save(self):
         """Serializes __objects to the JSON file (path: __file_path)"""
         serialized_objects = {}
         for key, obj in self.__objects.items():
             serialized_objects[key] = obj.to_dict()
-        with open(self.__file_path, "w", encoding="UTF8") as file:
-            json.dumps(serialized_objects, file)
+        with open(self.__file_path, "w",encoding="UTF8") as file:
+            json.dump(serialized_objects, file)
 
     def reload(self):
         """Deserializes the JSON file to __objects"""
-        try:
-            with open(FileStorage.__file_path, "r") as f:
-                objects_as_dict = json.load(f)
-                FileStorage.__objects = {
-                    obj_id: eval(obj_dict["__class__"])(**obj_dict)
-                    for obj_id, obj_dict in objects_as_dict.items()}
-        except FileNotFoundError:
-            pass
+        if os.path.isfile(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
+                try:
+                    obj_dict = json.load(file)
+
+                    for key, value in obj_dict.items():
+                        class_name, obj_id = key.split('.')
+
+                        cls = eval(class_name)
+
+                        instance = cls(**value)
+
+                        FileStorage.__objects[key] = instance
+                except Exception:
+                    pass
