9fa9efa6d638d9d2470872f64ac5f94d1732d4af
Razig17 <abdelrazig.sharif1@gmail.com>
2024-03-23 15:13:17 +0300

    Upload base_model file

--- models/base_model.py
+++ models/base_model.py
@@ -31,7 +31,7 @@ class BaseModel():
 
     def __str__(self):
         """Returns [<class name>] (<self.id>) <self.__dict__>"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
         """Updates the public instance attribute updated_at
