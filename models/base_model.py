e1566f252cf9919e1194272cf66eb94f74dc71d8
Razig17 <abdelrazig.sharif1@gmail.com>
2024-03-24 03:03:40 +0300

    Update base_model file

--- models/base_model.py
+++ models/base_model.py
@@ -5,23 +5,17 @@ This module contains BaseModule class that
 defines all common attributes/methods for other classes
 """
 
-import uuid
-from datetime import datetime
 
 import models
+import uuid
+from datetime import datetime
 
 
 class BaseModel:
     """Defines all common attributes/methods for other classes"""
 
     def __init__(self, *args, **kwargs):
-        """
-        Initialize instances of the BaseModel class.
-
-        Args:
-            *args: Variable length argument list.
-            **kwargs: Arbitrary keyword arguments.
-        """
+        """Initialize instances of the BaseModel class"""
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
