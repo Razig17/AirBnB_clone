2767240544e80c73fc0c289a969c830e250fbc43
Razig17 <abdelrazig.sharif1@gmail.com>
2024-03-25 03:05:18 +0300

    Upload user file

---
+++ models/user.py
@@ -0,0 +1,22 @@
+#!/ur/bin/python3
+
+""" This module contains the user class"""
+
+from .base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+    The user class
+
+    Attributes:
+        email (str): The user's email
+        password (str): The user's password
+        first_name (str): The user's first_name
+        last_name (str): The user's last_name
+
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
